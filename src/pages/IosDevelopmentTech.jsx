f1cdd199df382dd2180f2e6e6df6fe3076afb2ab
Abhishek <abhishekboadgurjar@gmail.com>
2025-02-07 15:10:21 +0530

    Update Technology Structure

--- src/pages/IosDevelopmentTech.jsx
+++ src/pages/IosDevelopmentTech.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Body from "../container/Technology/Frontend/MobileAppDev/Body";
-import Hero from "../container/Technology/Frontend/MobileAppDev/Hero";
+import Body from "../container/Technology/MobileAppDev/IOS Devlopment/Body";
+import Hero from "../container/Technology/MobileAppDev/IOS Devlopment/Hero";
 
 const IosDevelopmentTech = () => {
   return (
