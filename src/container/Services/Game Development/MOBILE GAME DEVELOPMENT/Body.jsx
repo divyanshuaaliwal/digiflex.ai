0c0f2049275306865652665211096d827b7efbe2
Adarsh saxena <adarshsaxena74411@gmail.com>
2025-02-13 18:09:18 +0530

    game devlopment updated

---
+++ src/container/Services/Game Development/MOBILE GAME DEVELOPMENT/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ServicesSection from './ServicesSection';
+import PricingPlans from './PricingPlans';
+import GameAceBenefits from './GameAceBenefits';
+const Body = () => {
+    return (
+       <>
+      <ServicesSection />
+      <PricingPlans/>
+      <GameAceBenefits />
+       </>
+    )
+}
+
+export default Body
